24e8e969c88b72064ec3f9881e0ab2392bd754bb
Tonny Brito <tonny@ferreiradebrito.com>
2018-09-20 17:10:19 -0300

    iniciando testes na calculadora

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        CalculadoraModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,12 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
 
 describe('CalculadoraService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: CalculadoraService = TestBed.get(CalculadoraService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [CalculadoraService]
+    });
   });
+
+  it('should ...', inject([CalculadoraService],
+    (service: CalculadoraService) => {
+    expect(service).toBeTruthy();
+  }));
+
+   it('deve garantir que 1 + 4 = 5',
+    inject([CalculadoraService], (service: CalculadoraService) => {
+      let soma = service.calcular(1, 4, CalculadoraService.SOMA);
+      expect(soma).toEqual(0);
+    })
+  );
 });
